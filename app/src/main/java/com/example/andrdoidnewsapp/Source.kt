d73531c3e47479818229786524dcda73fef0008f
Gin <57712394+ginderick@users.noreply.github.com>
2020-10-14 18:41:11 +0800

    Setting up Navigation

---
+++ app/src/main/java/com/example/andrdoidnewsapp/Source.kt
@@ -0,0 +1,6 @@
+package com.example.andrdoidnewsapp
+
+data class Source(
+    val id: Any,
+    val name: String
+)
---
+++ app/src/main/java/com/example/andrdoidnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.andrdoidnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY  = "c92e0f4f15474782b73ac220357e5f42"
+        const val BASE_URL  = "https://newsapi.org"
+    }
+}
